d94f121d135107ecc30fd11967d6d626f6e3ad1b
wuxin <myeagleandsnake@gaml.com>
2024-05-19 23:22:59 +0800

    feat：1

--- src/collctor.py
+++ src/collctor.py
@@ -3,8 +3,10 @@ import psutil
 
 # Create Prometheus Gauges
 disk_usage_gauge = Gauge('disk_usage_bytes', 'Disk usage in bytes', ['device', 'mount_point', 'type'])
+io_disk_usage_gauge = Gauge('io_disk_usage_bytes', 'Disk usage in bytes', ['device', 'type'])
 
 used_info_index = ["total", "used", "free", "percent"]
+io_used_info_index = ["read_count", "write_count", "read_bytes", "write_bytes", "read_time", "write_time"]
 
 
 # 获取所有的磁盘信息
@@ -36,9 +38,18 @@ def do_collect():
                 mount_point=disk.mountpoint,
                 type=index
             ).set(used_info.get(index))
+    # 获取写入写出量
+    io_used_info = psutil.disk_io_counters(perdisk=True)
+    for disk_mame, used_info in io_used_info.items():
+        for index in io_used_info_index:
+            io_disk_usage_gauge.labels(
+                device=disk_mame,
+                type=index
+            ).set(getattr(used_info, index))
 
 
 if __name__ == "__main__":
-    print(list(map(lambda ele: get_disk_usage(ele), get_all_disk_partitions())))
+    # print(list(map(lambda ele: get_disk_usage(ele), get_all_disk_partitions())))
+    print(psutil.disk_io_counters(perdisk=True))
     # do_collect()
     # print(disk_usage_gauge)
